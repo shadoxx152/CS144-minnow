f7be9497d2c1cf78d1a53232e6bbce1b126cdffa
shadoxx <shadoxx152@gmail.com>
2025-04-05 03:37:44 +0000

    check1 complete

--- apps/ip_raw.cc
+++ apps/ip_raw.cc
@@ -4,13 +4,13 @@ using namespace std;
 
 class RawSocket : public DatagramSocket
 {
-public:
-  RawSocket() : DatagramSocket( AF_INET, SOCK_RAW, IPPROTO_RAW ) {}
+  public:
+	RawSocket() : DatagramSocket( AF_INET, SOCK_RAW, IPPROTO_RAW ) {}
 };
 
 int main()
 {
-  // construct an Internet or user datagram here, and send using the RawSocket as in the Jan. 10 lecture
+	// construct an Internet or user datagram here, and send using the RawSocket as in the Jan. 10 lecture
 
-  return 0;
+	return 0;
 }
